59903de985caf1bcd300f58965e8d8cba4b0c5a4
rollgotobed <liuxu0420@163.com>
2020-02-12 11:45:37 +0800

    新增

---
+++ VS2.cpp
@@ -0,0 +1,65 @@
+﻿// VS2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int shuchu();
+double zhijiaosanjianxing(double a, double b, double c);
+
+
+int main()
+{
+    int aaa;
+    double bian;
+    bian = zhijiaosanjianxing(30.5,0,62);
+    if (bian != -1)
+    {
+        printf("%0.4f\n", bian);
+    }
+    else 
+    {
+        printf("传参有问题\n");
+    }
+    
+}
+
+int shuchu()
+{
+    printf("ffdkjs\n");
+    return 1;
+}
+
+/* 函数 zhijiaosanjianxing (double a, double b, double c)
+
+     功能：已知直角三角形两个边长，求第三边，a、b为直角边，c为斜边
+*/
+double zhijiaosanjianxing(double a, double b, double c)
+{
+    double k;
+    if ((a == 0 && b == 0) || (c == 0 && b == 0) || (a == 0 && c == 0) || (a == 0 && b == 0 && c==0) || (a != 0 && b != 0 && c != 0))
+    {
+        return -1;
+    }
+    if (a == 0)
+    {
+        return sqrt(c * c - b * b);
+    }
+    if (b == 0)
+    {
+        return sqrt(c * c - a * a);
+    }
+    if (c == 0)
+    {
+        return sqrt(b * b + a * a);
+    }
+}
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
